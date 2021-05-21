2ff71aefef5d0182a7db653663f44c9d40569787
basarab1504 <joker-power@yandex.ru>
2021-05-21 22:50:49 +0000

    cleaning + readmefix

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mai_user/hl_mai_lab_01
-# Build directory: /home/mai_user/hl_mai_lab_01
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtests "gtests")
