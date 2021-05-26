a3ba05d258a76281c3957db77b3309b5b4fa8773
basarab1504 <joker-power@yandex.ru>
2021-05-26 19:15:43 +0000

    init

--- web_server/http_request_factory.h
+++ web_server/http_request_factory.h
@@ -20,46 +20,44 @@
 #include "Poco/Util/HelpFormatter.h"
 #include <iostream>
 
-using Poco::Net::ServerSocket;
+using Poco::DateTimeFormat;
+using Poco::DateTimeFormatter;
+using Poco::ThreadPool;
+using Poco::Timestamp;
 using Poco::Net::HTTPRequestHandler;
 using Poco::Net::HTTPRequestHandlerFactory;
 using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerParams;
 using Poco::Net::HTTPServerRequest;
 using Poco::Net::HTTPServerResponse;
-using Poco::Net::HTTPServerParams;
-using Poco::Timestamp;
-using Poco::DateTimeFormatter;
-using Poco::DateTimeFormat;
-using Poco::ThreadPool;
-using Poco::Util::ServerApplication;
+using Poco::Net::ServerSocket;
 using Poco::Util::Application;
-using Poco::Util::Option;
-using Poco::Util::OptionSet;
-using Poco::Util::OptionCallback;
 using Poco::Util::HelpFormatter;
+using Poco::Util::Option;
+using Poco::Util::OptionCallback;
+using Poco::Util::OptionSet;
+using Poco::Util::ServerApplication;
 
 #include "handlers/person_handler.h"
 
-
-static bool startsWith(const std::string& str, const std::string& prefix)
+static bool startsWith(const std::string &str, const std::string &prefix)
 {
     return str.size() >= prefix.size() && 0 == str.compare(0, prefix.size(), prefix);
 }
 
-
-class HTTPRequestFactory: public HTTPRequestHandlerFactory
+class HTTPRequestFactory : public HTTPRequestHandlerFactory
 {
 public:
-    HTTPRequestFactory(const std::string& format):
-        _format(format)
+    HTTPRequestFactory(const std::string &format) : _format(format)
     {
     }
 
-    HTTPRequestHandler* createRequestHandler(
-        const HTTPServerRequest& request)
+    HTTPRequestHandler *createRequestHandler(
+        const HTTPServerRequest &request)
     {
-        static std::string person="/person"; 
-        if (startsWith(request.getURI(), person)) return new PersonHandler(_format);
+        static std::string person = "/person";
+        if (startsWith(request.getURI(), person))
+            return new PersonHandler(_format);
         return 0;
     }
 
